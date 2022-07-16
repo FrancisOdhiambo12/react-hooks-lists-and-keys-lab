b4051e9cb9dcc6377499be21e7077b5a55fb4604
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-07-16 23:58:47 +0300

    List and Keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+  const tech = technologies.map(technology => {
+    return (
+      <span key={technology}>{technology}</span>
+      );
+  })
+  
+    return (
+      <div className="project-item">
+        <h3>{name}</h3>
+        <p>{about}</p>
+        <div className="technologies">
+          {tech}
+        </div>
       </div>
-    </div>
-  );
-}
-
-export default ProjectItem;
+    );
+  }
+  
+  export default ProjectItem;
